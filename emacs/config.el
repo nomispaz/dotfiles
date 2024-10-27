456dfdbce4a4a682bb5e35b991c85baf6b72aad5
nomispaz <l0rdp4z1f1st@gmail.com>
2024-10-27 21:53:55 +0100

    emacs: catppuccin, use local packages only, eglot

--- emacs/config.el
+++ emacs/config.el
@@ -1,3 +1,5 @@
+(add-to-list 'load-path "~/.config/emacs/site-lisp")
+
 (setq backup-directory-alist '((".*" . "~/.local/share/emacs/backups")))
 
 (when (getenv "WAYLAND_DISPLAY")
@@ -17,20 +19,7 @@
 (setq interprogram-cut-function 'wl-copy)
 (setq interprogram-paste-function 'wl-paste))
 
-(unless (package-installed-p 'use-package)
-   (package-install 'use-package))
-
-(require 'package)
-
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
-
 (electric-pair-mode 1)
-
-(use-package catppuccin-theme
-:ensure t
-:config
-(load-theme 'catppuccin :no-confirm))
 
 (setq inhibit-startup-screen t)
 
@@ -51,52 +40,48 @@
 (global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
 (global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
 
-(use-package nerd-icons
-  :ensure t)
+(require 'doom-modeline)
+  (doom-modeline-mode 1)
 
-(use-package doom-modeline
-      :ensure t
-      :init (doom-modeline-mode 1))
+(require 'catppuccin-theme)
+    (load-theme 'catppuccin :no-confirm)
+  (catppuccin-load-flavor 'mocha)
+(catppuccin-reload)
 
-(use-package desktop
-  :init (desktop-save-mode 1)
-  :config
-   ;; don't save the following buffers
+(recentf-mode 1)
+
+(setq history-length 25)
+(savehist-mode 1)
+
+(save-place-mode 1)
+
+(require 'desktop)
+(desktop-save-mode 1)
+;; don't save the following buffers
    (add-to-list 'desktop-modes-not-to-save 'dired-mode)
    (add-to-list 'desktop-modes-not-to-save 'Info-mode)
    (add-to-list 'desktop-modes-not-to-save 'info-lookup-mode)
    (add-to-list 'desktop-modes-not-to-save 'fundamental-mode)
    ;; specify dir to save session
-   desktop-dirname "~/emacs_session_backup"
+   desktop-dirname "~/.local/share/emacs/emacs_session_backup"
    desktop-base-file-name "desktop"
    desktop-base-lock-name "desktop.lock"
-   )
 
-(use-package evil
-    :ensure t
-    :init
-    (setq evil-want-integration t)
-    (setq evil-want-keybinding nil)
-    :config
-    (evil-mode 1)
-    )
-
-(evil-set-undo-system 'undo-redo)
-
-  (use-package evil-collection
-    :after evil
-    :ensure t
-    :config
-    (evil-collection-init))
-
+(setq evil-want-keybinding nil)
+(require 'evil)
+     (require 'evil-collection)
+     (setq evil-want-integration t)
+    (evil-mode 1) 
+  (evil-set-undo-system 'undo-redo)
+  (evil-collection-init)
 ;; Using RETURN to follow links in Org/Evil 
-;; Unmap keys in 'evil-maps if not done, (setq org-return-follows-link t) will not work
-(with-eval-after-load 'evil-maps
-  (define-key evil-motion-state-map (kbd "SPC") nil)
-  (define-key evil-motion-state-map (kbd "RET") nil)
-  (define-key evil-motion-state-map (kbd "TAB") nil))
-;; Setting RETURN key in org-mode to follow links
-  (setq org-return-follows-link  t)
+  ;; Unmap keys in 'evil-maps if not done, (setq org-return-follows-link t) will not work
+  (with-eval-after-load 'evil-maps
+    (define-key evil-motion-state-map (kbd "SPC") nil)
+    (define-key evil-motion-state-map (kbd "RET") nil)
+    (define-key evil-motion-state-map (kbd "TAB") nil))
+  ;; Setting RETURN key in org-mode to follow links
+    (setq org-return-follows-link  t)
 
 ;; set leader key in all states
    (evil-set-leader nil (kbd "SPC"))
@@ -131,166 +116,125 @@
 
    ;; flycheck
    (define-key evil-normal-state-map (kbd "<leader> l l") '("Show list of flycheck errors" . flymake-show-buffer-diagnostics))
-   (define-key evil-normal-state-map (kbd "<leader> l n") '("Next flycheck error" . flycheck-next-error))
-   (define-key evil-normal-state-map (kbd "<leader> l p") '("Previous flycheck error" . flycheck-previous-error))
+   (define-key evil-normal-state-map (kbd "<leader> l n") '("Next flycheck error" . flymake-goto-next-error))
+   (define-key evil-normal-state-map (kbd "<leader> l p") '("Previous flycheck error" . flymake-goto-previous-error))
 
   ;; lsp
-   (define-key evil-normal-state-map (kbd "<leader> g r n") '("Rename variable or function" . lsp-rename))
-(define-key evil-normal-state-map (kbd "<leader> g d") '("LSP goto definition" . lsp-find-definition))
-(define-key evil-normal-state-map (kbd "<leader> g D") '("LSP Find references" . lsp-find-references))
-(define-key evil-normal-state-map (kbd "<leader> g s") '("LSP show doc in popup" . lsp-ui-doc-glance))
-(define-key evil-normal-state-map (kbd "C-.") '("LSP execute code action" . lsp-execute-code-action))
+   (define-key evil-normal-state-map (kbd "<leader> g r n") '("Rename variable or function" . eglot-rename))
+(define-key evil-normal-state-map (kbd "<leader> g d") '("LSP goto definition" . xref-find-definitions))
+(define-key evil-normal-state-map (kbd "<leader> g D") '("LSP Find references" . xref-find-references))
+(define-key evil-normal-state-map (kbd "<leader> g s") '("LSP show doc in buffer" . eldoc))
+(define-key evil-normal-state-map (kbd "C-.") '("LSP execute code action" . eglot-code-actions))
 
-(use-package which-key
-  :ensure t
-  :init
-  (which-key-mode 1)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3)
+(require 'which-key)
+(which-key-mode 1)
+   (setq which-key-idle-delay 0.3)
   (setq which-key-allow-evil-operators t)
-  )
 
-(use-package lsp-mode
-	:ensure t
-)
+(require 'eglot)
 
-(use-package lsp-ui
-  :ensure t
-  :hook (lsp-mode . lsp-ui-mode)
-)
-
-; Enable lsp-mode for Go and Rust modes
-(use-package go-mode
-  :ensure t
-  :after lsp-mode
-  :init
-  (add-hook 'go-mode-hook #'lsp)
-  (setq indent-tabs-mode nil)
+; Enable eglot for Go and Rust modes
+(require 'go-mode)
+ (setq indent-tabs-mode nil)
   (setq go-announce-deprecations t)
-  (setq go-mode-treesitter-derive t))
+ (setq go-mode-treesitter-derive t)
 
+(add-hook 'go-mode-hook 'eglot-ensure)
 (add-hook 'go-mode-hook 'yas-minor-mode)
 
-(use-package rust-mode
-  :ensure t
-  :after lsp-mode
-  :init
-  (add-hook 'rust-mode-hook #'lsp)
+(require 'rust-mode)
   (setq indent-tabs-mode nil)
-  (setq rust-mode-treesitter-derive t))
+  ;;(setq rust-mode-treesitter-derive t)
 
+
+(add-hook 'rust-mode-hook 'eglot-ensure)
 (add-hook 'rust-mode-hook
           (lambda () (setq indent-tabs-mode nil)))
 (add-hook 'rust-mode-hook 'yas-minor-mode)
 (setq rust-format-on-save t)
 
-(use-package yasnippet
-    :ensure t
-    :hook ((lsp-mode . yas-minor-mode)))
-(use-package yasnippet-snippets
-  :ensure t)
+(require 'yasnippet)
+  (require 'yasnippet-snippets)
 (yas-global-mode 1)
-  (add-hook 'elisp-mode-hook 'yas-minor-mode)
-  (add-hook 'org-mode-hook 'yas-minor-mode)
-  (add-hook 'org-mode-hook 'org-superstar-mode)
+        (add-hook 'elisp-mode-hook 'yas-minor-mode)
+        (add-hook 'org-mode-hook 'yas-minor-mode)
+        (add-hook 'org-mode-hook 'org-superstar-mode)
 
 ; Enable company-mode with language server support
-    (use-package company
-      :ensure t
-      :custom
-      (company-minimum-prefix-length 2)
-    )
-    (add-hook 'after-init-hook 'global-company-mode)
-(add-to-list 'company-backends '(company-capf company-yasnippet))
+      (require 'company)
+                (setq company-minimum-prefix-length 2)
+      
+      (add-hook 'after-init-hook 'global-company-mode)
+  (add-to-list 'company-backends '(company-capf company-yasnippet company-files))
+ (add-hook 'eglot-managed-mode-hook (lambda ()
 
-(use-package consult
-  :ensure t
-  :config
-  (recentf-mode 1)
-)
+(add-to-list 'company-backends
+'(company-capf :with company-yasnippet))))
 
-(use-package marginalia
-  :ensure t
-  :config
-  (marginalia-mode 1)
-  )
+(require 'consult)
+    (recentf-mode 1)
 
-(use-package vertico
-  :ensure t
-  :config
-  (setq vertico-cycle t)
+(require 'marginalia)
+    (marginalia-mode 1)
+
+(require 'vertico)
+    (setq vertico-cycle t)
   (setq vertico-resize nil)
   (vertico-mode 1)
-  )
 
-(use-package orderless
-  :ensure t
-  :config
-  (setq completion-styles '(orderless basic))
-  )
+(require 'orderless)
+    (setq completion-styles '(orderless basic))
 
 (defun my/org-mode-setup()
-    ;; active automatic indentation
-    (org-indent-mode)
-    ;; proportially resize font
-    (variable-pitch-mode 1)
-    ;; automatically perform line wrap
-    (visual-line-mode 1)
-    )
-(defun my/org-font-setup()
-  ;; Replace list hyphen with dot
-  (font-lock-add-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-  ;;Set faces for heading levels.
-  (dolist (face '((org-level-1 . 1.2)
-                  (org-level-2 . 1.1)
-                  (org-level-3 . 1.05)
-                  (org-level-4 . 1.0)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "DejaVu Sans" :weight 'regular :height (cdr face)))
-;; Ensure that anything that should be fixed-pitch in Org files appears that way
-(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-(set-face-attribute 'org-code nil :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-table nil :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
-)
-
-(use-package org
-  :hook (org-mode . my/org-mode-setup)
-  :config
-  ;; replace "..." at the end of collapsed headlines
-  (setq org-ellipsis " ▾"
-	;; remove special characters used for bold, kursiv etc.
-	org-hide-emphasis-markers t)
-
-  (setq org-agenda-start-with-log-mode t)
-  (setq org-log-done 'time)
-  (setq org-log-into-drawer t)
-  ;; RETURN will follow links in org-mode files
-  (setq org-return-follows-link  t)  
-  ;; (setq org-agenda-files
-  ;; 	'("/mnt/nvme2/orgmode/")
-  ;; 	)
-  (my/org-font-setup)
-  :bind (;;copy link anker to clipboard, insert with C-c C-l
-	 ("C-c l" . org-stored-links)
-	 )
+  ;; active automatic indentation
+  (org-indent-mode)
+  ;; proportially resize font
+  (variable-pitch-mode 1)
+  ;; automatically perform line wrap
+  (visual-line-mode 1)
   )
 
-(use-package org-superstar
-  :ensure t
- )
+(require 'org)
+(add-hook 'org-mode 'my/org-mode-setup)
+   ;; replace "..." at the end of collapsed headlines
+    (setq org-ellipsis " ▾"
+          ;; remove special characters used for bold, kursiv etc.
+          org-hide-emphasis-markers t)
 
-(use-package org-agenda
-  :config
+    (setq org-agenda-start-with-log-mode t)
+    (setq org-log-done 'time)
+    (setq org-log-into-drawer t)
+    ;; RETURN will follow links in org-mode files
+    (setq org-return-follows-link  t)  
+    ;; (setq org-agenda-files
+    ;; 	'("/mnt/nvme2/orgmode/")
+    ;; 	)
+    (font-lock-add-keywords 'org-mode
+                            '(("^ *\\([-]\\) "
+                               (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+    ;;Set faces for heading levels.
+    (dolist (face '((org-level-1 . 1.2)
+                    (org-level-2 . 1.1)
+                    (org-level-3 . 1.05)
+                    (org-level-4 . 1.0)
+                    (org-level-5 . 1.1)
+                    (org-level-6 . 1.1)
+                    (org-level-7 . 1.1)
+                    (org-level-8 . 1.1)))
+      (set-face-attribute (car face) nil :font "DejaVu Sans" :weight 'regular :height (cdr face)))
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+    ;;:bind (;;copy link anker to clipboard, insert with C-c C-l
+      ;;     ("C-c l" . org-stored-links))
+
+(require 'org-superstar)
+
+(require 'org-agenda)
     (setq org-agenda-files (directory-files-recursively "/mnt/nvme2/data/orgmode" "\\.org$"))
-    )
--- emacs/init.el
+++ emacs/init.el
@@ -2,13 +2,13 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(spacemacs-theme)))
+ '(package-selected-packages
+   '(org-superstar orderless vertico marginalia consult company yasnippet-snippets yasnippet rust-mode go-mode which-key spacemacs-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
