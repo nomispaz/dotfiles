6a17cd43f19c418e390f4c8fa7aaaaa3652fcbe1
nomispaz <l0rdp4z1f1st@gmail.com>
2024-11-02 12:50:10 +0100

    emacs add elpa

---
+++ emacs/elpa/spacemacs-theme-20241025.1536/spacemacs-theme-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "spacemacs-theme" "20241025.1536"
+  "Color theme with a dark and light versions."
+  ()
+  :url "https://github.com/nashamri/spacemacs-theme"
+  :commit "398f73298c5ede1fffe8a7bc7efc40c2e89a754b"
+  :revdesc "398f73298c5e"
+  :keywords '("color" "theme"))
